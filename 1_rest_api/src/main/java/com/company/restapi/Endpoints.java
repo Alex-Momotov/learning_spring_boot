dbf2ad1744a9e08ee81de3f4381e7a087b2cbda4
Alex Momotov <alex.momotov@vonage.com>
2020-05-21 23:27:53 +0100

    Notes and example of rest api.

--- 1_rest_api/src/main/java/com/company/restapi/Endpoints.java
+++ 1_rest_api/src/main/java/com/company/restapi/Endpoints.java
@@ -1,15 +1,46 @@
 package com.company.restapi;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
+//----------------------------------------------------------------------------------------------------------------------
+// @RestController means the class is ready for use by Spring MVC to handle web requests. It combines @Controller and @ResponseBody
+// annotations that results in web requests returning data rather than a view.
+
+// @RequestMapping maps path '/' to the home() method and accepts GET http requests as specified by annotation parameters
+
+// @GetMapping is a convenience annotation that uses @RequestMapping(method = {RequestMethod.GET})
+// @PostMapping is a convenience annotation that uses @RequestMapping(method = {RequestMethod.POST})
+
+// @Request param takes in a query parameter
+// @RequestBody takes in a request body and automatically serialises it to class Person using Jackson
+
+
 
 @RestController
 public class Endpoints {
 
+    @RequestMapping(method = {RequestMethod.GET}, path = "/")
+    public String home() {
+        return "you are at home";
+    }
+
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+    }
+
+    @PostMapping("hi")
+    public String hi(@RequestBody Person person) {
+        return String.format("Hi %s %s", person.getName(), person.getSurname());
     }
 
 }
--- 1_rest_api/src/main/java/com/company/restapi/MyApplication.java
+++ 1_rest_api/src/main/java/com/company/restapi/MyApplication.java
@@ -1,7 +1,19 @@
 package com.company.restapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+//----------------------------------------------------------------------------------------------------------------------
+// @SpringBootApplication a convenience annotation that adds the following annotations:
+//      - @Configuration Marks a class as a source of bean definitions
+//      - @EnableAutoConfiguration Tells spring to start adding beans based on what is on classpath, other beans, and various property settings
+//      - @ComponentScan tells spring to look for other components, configurations, services in the same package letting it find the rest controller
+
 
 @SpringBootApplication
 public class MyApplication {
